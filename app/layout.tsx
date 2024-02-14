05577578fa1df90321cf7919c98c012527e277df
yeoheung00 <yeoheung27@gmail.com>
2024-02-14 16:55:28 +0900

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <h1>Nav</h1>
+          <Link href="/about">go to about</Link>
+          <Link href="/profile">go to profile</Link>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
